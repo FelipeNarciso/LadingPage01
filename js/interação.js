83032945bba61659293b8f5ff7ee3809e09ea8ac
FelipeNarciso <luisfelipenb88@gmail.com>
2024-06-10 17:57:28 -0300

    depoimentos page

--- js/interação.js
+++ js/interação.js
@@ -3,6 +3,14 @@ function contato(){
     let email = document.getElementById('txtEmail')
     let num = document.getElementById('txtTel')
 
+    if(isNaN(num.value)){
+        alert('Erro! Só são permitidos números no campo "Telefone"!')
+        nome.value = ''
+        email.value = ''
+        num.value = ''
+        return
+    }
+
     if(nome.value === ''){
         alert('Erro! Um ou mais campos não estão preenchidos')
     }else if(email.value === ''){
