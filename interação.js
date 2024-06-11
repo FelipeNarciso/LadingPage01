29ca80145f3c59ce9b014aa0608d29806b37d57d
FelipeNarciso <luisfelipenb88@gmail.com>
2024-06-11 14:59:02 -0300

    media queries

--- interação.js
+++ interação.js
@@ -26,6 +26,11 @@ function contato(){
 } 
 
 function menu(){
-    let menu = document.getElementById('menu-toggle')
     let nav = document.getElementById('navbar')
+    if(nav.style.display === 'block'){
+        nav.style.display = 'none'
+    }else{
+        nav.style.display = 'block'
+    }
+    
 }
